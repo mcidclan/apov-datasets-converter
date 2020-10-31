fa1bcdd9704e64ce314c01071c1a921471e04237
mcidclan <m.cid.clan@gmail.com>
2020-10-31 21:06:05 +0100

    -Implements the basics to convert slice from medical systems to voxels.bin used by the apov project.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apov-datasets-converter
-Convert classical datasets to the voxels.bin file entry used by the APoV generator.
+Experimental converter of sliced 3-Dimensional data sets, to the voxels.bin file
+entry used by the APoV generator. This current version has only been tested with
+MRbrain from graphics.stanford.edu for now.
+
---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <cstdio>
+#include <string>
+#include <dirent.h>
+
+#define i16 short int
+#define u8 unsigned char
+#define u32 unsigned int
+#define u16 unsigned short int
+
+template<typename T>
+struct Vec4 {
+    T x, y, z, w;
+};
+    
+struct Voxel {
+    u32 color;
+    Vec4<i16> coordinates;
+};
+
+static u16 SLICE_WIDTH = 256;
+static u16 SLICE_HEIGHT = 256;
+static u16 SLICE_COUNT = 1;
+static u8 THRESHOLD = 0;
+static u8 DEPTH_SIZE = 1;
+static u16 BIT_MASK = 0x000F;
+//static bool THRESHOLD_AUTO = false;
+static u32 SLICE_VOXEL_COUNT;
+static u32 DATA_VOXEL_COUNT;
+
+int main(int argc, char** argv) {
+    
+    int i = 2;
+    while(i < argc) {
+        const std::string name = argv[i];
+        if(name.find("slice-width:") == 0) {
+           SLICE_WIDTH = std::stoi(name.substr(12));
+        } else if(name.find("slice-height:") == 0) {
+           SLICE_HEIGHT = std::stoi(name.substr(13));
+        } else if(name.find("slice-count:") == 0) {
+           SLICE_COUNT = std::stoi(name.substr(12));
+        } else if(name.find("threshold:") == 0) {
+           THRESHOLD = std::stoi(name.substr(10));
+        } else if(name.find("depth-size:") == 0) {
+           DEPTH_SIZE = std::stoi(name.substr(11));
+        } else if(name.find("bit-mask:") == 0) {
+            //0x000F
+            BIT_MASK = stoll(name.substr(9), 0 , 16);
+        }
+        i++;
+    }
+    
+    
+    
+    DIR *dir;
+    struct dirent *ent;
+    if((dir = opendir(argv[1])) != NULL) {
+        printf("Scanning %s folder...\n", argv[1]);
+        SLICE_VOXEL_COUNT = SLICE_WIDTH * SLICE_HEIGHT;
+        DATA_VOXEL_COUNT = SLICE_VOXEL_COUNT * SLICE_COUNT;
+        u16* const data = new u16[DATA_VOXEL_COUNT];
+        
+        u16 ns = 0;
+        while((ent = readdir(dir)) != NULL) {
+            std::string name = ent->d_name;
+            if(name != ".." && name != ".") {
+                std::size_t pos = name.find_last_of(".");
+                if(pos != std::string::npos) {
+                    std::string filename = argv[1];
+                    filename += "/" + name;
+                    FILE* const f = fopen(filename.c_str(), "rb");
+                    if(f != NULL) {
+                        const u32 sid = std::stoi(name.substr(pos + 1)) - 1;
+                        const u32 offset = sid * SLICE_VOXEL_COUNT;
+                        printf("Slice id: %u. File (%s) loaded.\n", sid, name.c_str());
+                        fread(&(data[offset]), sizeof(u16), SLICE_VOXEL_COUNT, f);
+                        fclose(f);
+                        ns++;
+                    }
+                }
+            }
+        }
+        printf("%u files read\n", ns);
+        closedir(dir);
+        
+        const u16 SLICE_HALF_WIDTH = SLICE_WIDTH / 2;
+        const u16 SLICE_HALF_HEIGHT = SLICE_HEIGHT / 2;
+        const u16 SLICE_HALF_COUNT = SLICE_COUNT / 2;
+        
+        FILE* const f = fopen("voxels.bin", "wb");
+        if(f != NULL) {
+            u16 k = 0;
+            while(k < (SLICE_COUNT * DEPTH_SIZE)) {
+                u16 i = 0;
+                while(i < SLICE_WIDTH) {
+                    u16 j = 0;
+                    while(j < SLICE_HEIGHT) {
+                        const u16 value = data[i + j * SLICE_WIDTH + (k / DEPTH_SIZE)  * SLICE_VOXEL_COUNT];
+                        const u8 l = (value & BIT_MASK) << 4;
+                        
+                        if(l > THRESHOLD) {
+                            const u32 color = 0x000000FF | l << 8 | l << 16 | l << 24;
+                            
+                            const Voxel voxel = {
+                                color,
+                                {
+                                    (i16)(i - SLICE_HALF_WIDTH),
+                                    (i16)(j - SLICE_HALF_HEIGHT),
+                                    (i16)(k - (SLICE_HALF_COUNT * DEPTH_SIZE)),
+                                    1
+                                }
+                            };
+                            fwrite(&voxel, sizeof(Voxel), 1, f);
+                        }
+                        j++;
+                    }
+                    i++;
+                }                
+                printf("\rWrite %u / %u", k + 1, SLICE_COUNT * DEPTH_SIZE);
+                k++;
+            }
+            fclose(f);
+        }
+                
+        delete [] data;
+    }
+    return 0;
+}
